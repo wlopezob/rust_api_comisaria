436263ffb15d7cc90a390c54a56f469ddbee2fae
wlopezob <wlopezob@gmail.com>
2023-02-10 23:11:46 -0500

    readme

--- src/controller/comisaria_controller.rs
+++ src/controller/comisaria_controller.rs
@@ -2,7 +2,7 @@ use axum::{extract::State, routing::get, Json, Router};
 use dotenvy_macro::dotenv;
 
 use crate::{
-    models::api_exception::ApiException,
+    models::{api_exception::ApiException, comisaria_document::ComisariaDocument},
     routes::init::AppState,
     services::{comisaria_service::ComisariaService},
 };
@@ -11,14 +11,14 @@ pub fn comisaria_controller() -> Router<AppState> {
     Router::new().route("/getall", get(get_all))
 }
 
-async fn get_all(State(app_state): State<AppState>) -> Result<Json<bool>, ApiException> {
+async fn get_all(State(app_state): State<AppState>) -> Result<Json<Vec<ComisariaDocument>>, ApiException> {
     let url_comisaria_count = dotenv!("URL_COMISARIA_COUNT");
     let url_comisaria = dotenv!("URL_COMISARIA");
     let host = dotenv!("HOST");
     let origin = dotenv!("ORIGIN");
 
-    ComisariaService::new(app_state.comisaria_repository.clone())
+    let comisarias = ComisariaService::new(app_state.comisaria_repository.clone())
         .get_all_comisaria(url_comisaria_count, url_comisaria, host, origin)
         .await?;
-    Ok(Json(true))
+    Ok(Json(comisarias))
 }
